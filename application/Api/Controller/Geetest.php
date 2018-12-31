2c5c00ab4da9b1b01d9de6ce9cd75471d4825d6e
Q <2772002035@qq.com>
2018-12-31 20:54:58 +0800

    Change

--- application/Api/Controller/Geetest.php
+++ application/Api/Controller/Geetest.php
@@ -14,26 +14,20 @@ use think\Controller;
 
 class Geetest extends Controller
 {
-
-    private $GtSdk;
-
-    public function _initialize(){
-        $this -> GtSdk = new GeetestLib(env('GeeId'),env('GeeKey'));
-    }
-
+    
     //使用GET返回challenge和captcha_id
     public function GetSteam(){
-
+        $geetest = new GeetestLib(env('GeeId'),env('GeeKey'));
         session_start();
         $data = array(
             "user_id" => 'test',
             "client_type" => "web",
             "ip_address" => "127.0.0.1"
         );
-        $status = $this -> GtSdk ->pre_process($data, 1);
+        $status = $geetest -> pre_process($data, 1);
         $_SESSION['gtserver'] = $status;
         $_SESSION['user_id'] = $data['user_id'];
-        echo $this -> GtSdk ->get_response_str();
+        echo $geetest -> get_response_str();
     }
 
     //输出二次验证结果
@@ -41,7 +35,7 @@ class Geetest extends Controller
         $geetest = new GeetestLib(env('GeeId'),env('GeeKey'));
         $user_id = $_SESSION['user_id'];
         if ($_SESSION['gtserver'] == 1){
-            $result = $this -> GtSdk -> success_validate($_POST['geetest_challenge'].$_POST('geetest_validate'),$_POST['geetest_seccode']);
+            $result = $geetest-> success_validate($_POST['geetest_challenge'].$_POST('geetest_validate'),$_POST['geetest_seccode']);
         }
     }
 
