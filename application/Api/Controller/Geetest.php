e05c4bac846ca5c083570e2619d5b1c4bf6d37f9
Q <2772002035@qq.com>
2018-12-31 20:39:20 +0800

    Change

--- application/Api/Controller/Geetest.php
+++ application/Api/Controller/Geetest.php
@@ -14,18 +14,31 @@ use think\Controller;
 
 class Geetest extends Controller
 {
+    public function __initialize(){
+        $this -> geetest = new GeetestLib(env('GeeId'),env('GeeKey'));
+    }
+
+    //使用GET返回challenge和captcha_id
     public function GetSteam(){
-        $geetest = new GeetestLib(env('GeeId'),env('GeeKey'));
+
         session_start();
         $data = array(
             "user_id" => 'test',
             "client_type" => "web",
             "ip_address" => "127.0.0.1"
         );
-        $status = $geetest->pre_process($data, 1);
+        $status = $this -> geetest->pre_process($data, 1);
         $_SESSION['gtserver'] = $status;
         $_SESSION['user_id'] = $data['user_id'];
-        echo $geetest->get_response_str();
+        echo $this -> geetest->get_response_str();
+    }
+
+    //输出二次验证结果
+    public function VerifyLoginServlet(){
+        $user_id = $_SESSION['user_id'];
+        if ($_SESSION['gtserver'] == 1){
+            $result = $this -> geetest -> success_validate($_POST['geetest_challenge'].$_POST('geetest_validate'),$_POST['geetest_seccode']);
+        }
     }
 
 }
