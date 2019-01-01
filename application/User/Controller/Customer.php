f0855f3c3dd9fc9186cd8e63454fc26dde52b70c
Q <2772002035@qq.com>
2019-01-01 22:31:09 +0800

    Change

---
+++ application/User/Controller/Customer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qianchao
+ * Date: 2019-01-01
+ * Time: 22:29
+ */
+
+namespace app\User\Controller;
+
+
+use think\Controller;
+
+class Customer extends Controller
+{
+    protected $middleware = ['\app\http\middleware\Auth'];
+
+    public function index(){
+        
+    }
+
+}
