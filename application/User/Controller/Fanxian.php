74e8af7869e0941e52b2ec548e76ae1c6e893f0e
Q <2772002035@qq.com>
2019-01-01 22:50:48 +0800

    Change

---
+++ application/User/Controller/Fanxian.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qianchao
+ * Date: 2019-01-01
+ * Time: 22:35
+ */
+
+namespace app\User\Controller;
+
+
+use think\Controller;
+
+class Fanxian extends Controller
+{
+    protected $middleware = ['\app\http\middleware\Auth'];
+
+    public function index(){
+        return $this -> display();
+    }
+
+}
