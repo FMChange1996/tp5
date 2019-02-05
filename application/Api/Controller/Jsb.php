95654458bf60bf348e842b00404d45c3d79f25b2
Q <2772002035@qq.com>
2019-02-05 20:52:39 +0800

    新增用户跟踪功能

--- application/Api/Controller/Jsb.php
+++ application/Api/Controller/Jsb.php
@@ -15,7 +15,7 @@ use HttpClient;
 class Jsb extends Controller
 {
     public function test($tid){
-        $client = new HttpClient('2b42ce7e77c18ff6f04c32c15325c14a6810dfc1166d1015d928d2fee82d05ec','218170bd06502f327545cc53680b71ba7629746e9aa1d1a7fa847c983db3a360');
+        $client = new HttpClient('','');
         $client -> put('/JSB/rest/logistics/LogisticsDummySendRequest?tid='.$tid);
         return $client->status.$client->getContent();
     }
