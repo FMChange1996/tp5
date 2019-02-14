dbbf6102b110689100484779467275747eda07a6
Mimic <2772002035@qq.com>
2019-02-14 17:01:19 +0800

    修复部分逻辑

--- application/user/controller/Customerservice.php
+++ application/user/controller/Customerservice.php
@@ -11,6 +11,10 @@ namespace app\User\controller;
 
 use app\user\command\Base;
 use app\user\model\After;
+use think\facade\Request;
+use think\facade\Validate;
+use think\facade\Session;
+
 
 class Customerservice extends Base
 {
@@ -23,6 +27,45 @@ class Customerservice extends Base
 
     public function PayOut(){
         return view('customerservice/pay_out',['title' => '售后支出']);
+    }
+
+    public function AddAfter(){
+        if (Request::isPost()){
+            $data = [
+                'wangwang' => Request::param('wangwang'),
+                'text' => Request::param('text')
+            ];
+
+            $message = [
+                'wangwang.require' => '旺旺名不能为空',
+                'text.require' => '事件不能为空'
+            ];
+
+            $validate = Validate::make([
+                'wangwang' => 'require',
+                'text' => 'require'
+            ],$message);
+
+            if (!$validate -> check($data)){
+                return json(['code' => 400 , 'message' => $validate -> getError()]);
+            }else{
+                $after = new After([
+                    'wangwang' => $data['wangwang'],
+                    'text' => $data['text'],
+                    'status' => 0,
+                    'create' => Session::get('username')
+                ]);
+                if ($after -> save()){
+                    return json(['code' => 200 , 'message' => '添加成功！']);
+                }else{
+                    return json(['code' => 400 , 'message' => '添加失败！']);
+                }
+            }
+        }elseif (Request::isGet()){
+            return view('customerservice/add_after',['title' => '添加售后']);
+        }else{
+            return $this -> error('访问错误！');
+        }
     }
 
 
